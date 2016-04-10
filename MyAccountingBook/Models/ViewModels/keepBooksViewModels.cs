e9f367ac1265d6d89cdc143ef658c0eaec300b45
jame2408 <jame2408@gmail.com>
2016-04-10 16:13:37 +0800

    add 『Display attribute』 and 『placeholder』

--- MyAccountingBook/Models/ViewModels/keepBooksViewModels.cs
+++ MyAccountingBook/Models/ViewModels/keepBooksViewModels.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace MyAccountingBook.Models.ViewModels
 {
     public class keepBooksViewModels
-    {     
-
+    {
+        [DisplayName("類別")]
         public InOutList InOut { get; set; }
+        [DisplayName("金額")]
         public string Amount { get; set; }
+        [DisplayName("日期")]
         public string Date { get; set; }
+        [DisplayName("備註")]
         public string Memo { get; set; }
 
     }

f929001489e728d6eb8add6dbd8b3f7623dc45d4
jame2408 <jame2408@gmail.com>
2016-04-10 16:48:33 +0800

    依建議調整ChildAction與下拉式選單寫法

--- MyAccountingBook/Models/ViewModels/keepBooksViewModels.cs
+++ MyAccountingBook/Models/ViewModels/keepBooksViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace MyAccountingBook.Models.ViewModels
     public class keepBooksViewModels
     {
         [DisplayName("類別")]
+        [Range(1, int.MaxValue)]
         public InOutList InOut { get; set; }
         [DisplayName("金額")]
         public string Amount { get; set; }
@@ -19,9 +21,9 @@ namespace MyAccountingBook.Models.ViewModels
 
     }
     public enum InOutList
-    {        
-        支出,
-        收入
+    {
+        支出 = 1,
+        收入 = 2
     }
 
 

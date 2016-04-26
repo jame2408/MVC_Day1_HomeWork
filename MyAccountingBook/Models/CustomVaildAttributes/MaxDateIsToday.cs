0594375cd6acd7adec2a27c5ef712430f4bcc0f0
jame2408 <jame2408@gmail.com>
2016-04-26 23:41:59 +0800

    增加『金額欄位正整數驗證』、『日期格式與不能輸入大於今日日期之驗證』、與『備註不能超過120字之驗證』

---
+++ MyAccountingBook/Models/CustomVaildAttributes/MaxDateIsToday.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyAccountingBook.Models.CustomVaildAttributes
+{
+    public class MaxDateIsToday : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if ((DateTime)value <= Convert.ToDateTime(DateTime.Today.ToShortDateString()))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("日期不能大於今天.");
+            }
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
--- MyAccountingBook/Models/ViewModels/keepBooksViewModels.cs
+++ MyAccountingBook/Models/ViewModels/keepBooksViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyAccountingBook.Models.CustomVaildAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -11,16 +12,25 @@ namespace MyAccountingBook.Models.ViewModels
     {
         //[Required]
         [DisplayName("類別")]
-        [Range(1, int.MaxValue, ErrorMessage = "請選擇類別")]
+        [Range(1, int.MaxValue, ErrorMessage = "請選擇{0}")]
         public InOutList InOut { get; set; }
-        [Required]
+
         [DisplayName("金額")]
+        [Required]
+        [DisplayFormat(DataFormatString ="{0:N0}")]
+        [Range(1, int.MaxValue, ErrorMessage ="{0}需為正整數")]
         public int Amount { get; set; }
-        [Required]
+
         [DisplayName("日期")]
-        public DateTime Date { get; set; }
         [Required]
+        [DataType(DataType.Date)]
+        [MaxDateIsToday]
+        //[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]        
+        public DateTime Date { get; set; }
+
         [DisplayName("備註")]
+        [StringLength(120, ErrorMessage ="{0}不能超過120個字")]
+        [Required]
         public string Memo { get; set; }
 
     }
